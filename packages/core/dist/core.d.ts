684994fd617c8c49e95144cb0861a22b50aee6fa
Dmitry Bayko <dabaycode@gmail.com>
2022-01-21 15:03:52 +1000

    add types to core

---
+++ packages/core/dist/core.d.ts
@@ -0,0 +1,6 @@
+import '@storysdk/react/dist/bundle.css';
+export declare class Story {
+    token: string;
+    constructor(token: string);
+    renderGroups(element?: Element | HTMLDivElement | null): void;
+}
---
+++ packages/core/dist/services/API/index.d.ts
@@ -0,0 +1,126 @@
+export declare const API: {
+    apps: {
+        getList(): import("axios").AxiosPromise<any>;
+    };
+    groups: {
+        getList(params: {
+            appId: string;
+        }): import("axios").AxiosPromise<any>;
+    };
+    stories: {
+        getList(params: {
+            appId: string;
+            groupId: string;
+        }): import("axios").AxiosPromise<any>;
+    };
+    statistics: {
+        group: {
+            sendDuration(params: {
+                groupId: string;
+                uniqUserId: string;
+                seconds: number;
+            }): import("axios").AxiosPromise<any>;
+            onOpen(params: {
+                groupId: string;
+                uniqUserId: string;
+            }): import("axios").AxiosPromise<any>;
+            onClose(params: {
+                groupId: string;
+                uniqUserId: string;
+            }): import("axios").AxiosPromise<any>;
+        };
+        story: {
+            sendDuration(params: {
+                groupId: string;
+                storyId: string;
+                uniqUserId: string;
+                seconds: number;
+            }): import("axios").AxiosPromise<any>;
+            onOpen(params: {
+                groupId: string;
+                storyId: string;
+                uniqUserId: string;
+            }): import("axios").AxiosPromise<any>;
+            onClose(params: {
+                groupId: string;
+                storyId: string;
+                uniqUserId: string;
+            }): import("axios").AxiosPromise<any>;
+            onNext(params: {
+                groupId: string;
+                storyId: string;
+                uniqUserId: string;
+            }): import("axios").AxiosPromise<any>;
+            onPrev(params: {
+                groupId: string;
+                storyId: string;
+                uniqUserId: string;
+            }): import("axios").AxiosPromise<any>;
+        };
+        widgets: {
+            chooseAnswer: {
+                onAnswer(params: {
+                    widgetId: string;
+                    storyId: string;
+                    groupId: string;
+                    answer: string;
+                    uniqUserId: string;
+                }): import("axios").AxiosPromise<any>;
+            };
+            clickMe: {
+                onClick(params: {
+                    widgetId: string;
+                    storyId: string;
+                    groupId: string;
+                    uniqUserId: string;
+                    url: string;
+                }): import("axios").AxiosPromise<any>;
+            };
+            emojiReaction: {
+                onReact(params: {
+                    widgetId: string;
+                    storyId: string;
+                    groupId: string;
+                    uniqUserId: string;
+                    emoji: string;
+                }): import("axios").AxiosPromise<any>;
+            };
+            question: {
+                onAnswer(params: {
+                    widgetId: string;
+                    storyId: string;
+                    groupId: string;
+                    uniqUserId: string;
+                    answer: string;
+                }): import("axios").AxiosPromise<any>;
+            };
+            slider: {
+                onSlide(params: {
+                    widgetId: string;
+                    storyId: string;
+                    groupId: string;
+                    uniqUserId: string;
+                    value: number;
+                }): import("axios").AxiosPromise<any>;
+            };
+            swipeUp: {
+                onSwipe(params: {
+                    widgetId: string;
+                    storyId: string;
+                    groupId: string;
+                    uniqUserId: string;
+                    url: string;
+                }): import("axios").AxiosPromise<any>;
+            };
+            talkAbout: {
+                onAnswer(params: {
+                    widgetId: string;
+                    storyId: string;
+                    groupId: string;
+                    uniqUserId: string;
+                    answer: string;
+                }): import("axios").AxiosPromise<any>;
+            };
+        };
+    };
+};
