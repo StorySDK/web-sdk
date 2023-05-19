1b51e8b9e049583e746d0f61d86835e94a8c9146
Dmitry Bayko <dabaycode@gmail.com>
2023-05-19 13:21:58 -0300

    fix import

--- packages/react/dist/types/index.d.ts
+++ packages/react/dist/types/index.d.ts
@@ -3,4 +3,4 @@ export * from './WidgetComponent';
 export * from './WidgetType';
 export * from './GroupType';
 export * from './StoryContext';
-export * from './WidgetElementsTypes';
+export * from './widgetElementsTypes';
--- packages/react/types/WidgetType.ts
+++ packages/react/types/WidgetType.ts
@@ -10,7 +10,7 @@ import {
   QuizRateWidgetElementsType,
   SliderWidgetElementsType,
   TalkAboutElementsType
-} from './WidgetElementsTypes';
+} from './widgetElementsTypes';
 import {
   ChooseAnswerWidgetParamsType,
   ClickMeWidgetParamsType,
