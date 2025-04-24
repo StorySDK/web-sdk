5e752da41874002419df5c36dcf8d8e345895395
Dmitry Bayko <dabaycode@gmail.com>
2025-04-24 15:32:21 +0300

    separate react-native

--- packages/react-native/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { StoryGroups } from './StoryGroups.js';
-import { StoryModal } from './StoryModal.js';
-export { StoryGroups, StoryModal };
-export * from './StoryModal';
-export * from './StoryGroups';
-export * from './StorageHandler';
--- packages/react-native/src/StoryGroups.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var StoryGroups_1 = require("./StoryGroups.tsx");
-exports.StoryGroups = StoryGroups_1.StoryGroups; 
